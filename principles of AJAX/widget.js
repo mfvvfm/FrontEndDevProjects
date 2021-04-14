2c39aedf4e5edb1e72df332d19009ccc9ecb4475
mfvvfm <38672042+mfvvfm@users.noreply.github.com>
2021-04-15 00:45:15 +0100

    adding room request

--- principles of AJAX/widget.js
+++ principles of AJAX/widget.js
@@ -1,23 +1,41 @@
 var xhr = new XMLHttpRequest();
-
 xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-      var employees = JSON.parse(xhr.responseText);
-      // JSON.parse takes a string and converts into JS obj
-      // Text response from server is JSON formatted string from employees.json file
-      var statusHTML = '<ul class="bulleted">';
-    for (var i = 0; i < employees.length; i += 1) {
+  if(xhr.readyState === 4) {
+    var employees = JSON.parse(xhr.responseText);
+    var statusHTML = '<ul class="bulleted">';
+    for (var i=0; i<employees.length; i += 1) {
       if (employees[i].inoffice === true) {
         statusHTML += '<li class="in">';
       } else {
         statusHTML += '<li class="out">';
       }
-     statusHTML += employees[i].name;
-     statusHTML += '</li>';
+      statusHTML += employees[i].name;
+      statusHTML += '</li>';
     }
     statusHTML += '</ul>';
     document.getElementById('employeeList').innerHTML = statusHTML;
   }
 };
-xhr.open('GET', 'data/employees.json');
+xhr.open('GET', '../data/employees.json');
 xhr.send();
+
+var roomRequest = new XMLHttpRequest();
+roomRequest.onreadystatechange = function () {
+  if(xhr.readyState === 4) {
+    var rooms = JSON.parse(roomRequest.responseText);
+    var statusHTML = '<ul class="rooms">';
+    for (var i=0; i<rooms.length; i += 1) {
+      if (rooms[i].available === true) {
+        statusHTML += '<li class="empty">';
+      } else {
+        statusHTML += '<li class="full">';
+      }
+      statusHTML += rooms[i].room;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML;
+  }
+};
+roomRequest.open('GET', '../data/rooms.json');
+roomRequest.send();
