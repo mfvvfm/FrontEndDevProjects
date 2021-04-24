b3e8b6bc11c93822c289932201cdf06a88134166
mfvvfm <38672042+mfvvfm@users.noreply.github.com>
2021-04-25 00:31:46 +0100

    with constructor method and super

--- React/ScoreboardApp/app.js
+++ React/ScoreboardApp/app.js
@@ -42,19 +42,33 @@ const Player = (props) => {
         { props.name }
       </span>
 
-      <Counter score={ props.score }/>
+      <Counter />
     </div>
   );
 }
+// In JS classes, extends keyword is used to create a subclass or child of another class
+// We are extending from React.Component part of React's API for component class definition
+// this refers to component instance
+class Counter extends React.Component {
+  constructor() {
+    /* Super calls the constructor of the component class we're extending */
+    super()
+    this.state = {
+      /* Since state is data that changes over time, we first need to set an intial state, the state is our Counter
+        is going to be the score we want to display for each player*/
+        score: 0
+    };
+  }
 
-const Counter = (props) => {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <span className="counter-score">{ props.score }</span>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">{ this.state.score }</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+  }
 }
 
 const App = (props) => {
@@ -67,20 +81,20 @@ const App = (props) => {
 
       {/* Players list */}
       {/* this maps players into player parameter to represent current item being processed in a new array */}
+      {/* This are the names and score from each player item we're getting from map */}
       { props.initialPlayers.map( player =>
         <Player
-          {/* This are the names and score from each player item we're getting from map */}
-          name={ player.name }
-          score={ player.score }
-          key={ player.id.toString() }
+          name={player.name}
+          score={player.score}
+          key={player.id.toString()}
         />
       )}
     </div>
   );
 }
 
+{/* prop initialPlayers={players} passes array into App */}
 ReactDOM.render(
-  {/* prop initialPlayers={players} passes array into App */}
   <App initialPlayers={ players }/>,
   document.getElementById('root')
 );
